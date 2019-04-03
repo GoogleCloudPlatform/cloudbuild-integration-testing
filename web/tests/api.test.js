9c682bf8cbdb30c68f9f64c8751fc60c821320d9
Guillermina Muro <guillermina.muro@gmail.com>
2019-04-03 16:01:23 -0700

    added deps fixed indents

--- web/tests/api.test.js
+++ web/tests/api.test.js
@@ -7,20 +7,20 @@ const app = require('../app');
 chai.use(chaiHttp);
 
 describe('server', () => {
-    it('it should be listening', (done) => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-                done();
-            });
-    });
-    it('it should 404 on nonexistent route', (done) => {
-        chai.request(app)
-            .get('/test')
-            .end((err, res) => {
-                res.should.have.status(404);
-                done();
-            });
-    });
+  it('it should be listening', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('it should 404 on nonexistent route', (done) => {
+    chai.request(app)
+      .get('/test')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
 });
